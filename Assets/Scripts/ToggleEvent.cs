db40f64640fe2de98d49e530eb91aa56dfb814e4
Jacob van't Hoog <vantjac@gmail.com>
2020-02-01 16:57:58 -0800

    Add split screen toggle

---
+++ Assets/Scripts/ToggleEvent.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ToggleEvent : MonoBehaviour
+{
+    public UnityEvent on, off;
+
+    public void Toggle(bool toggle)
+    {
+        if (toggle)
+            on.Invoke();
+        else
+            off.Invoke();
+    }
+}
