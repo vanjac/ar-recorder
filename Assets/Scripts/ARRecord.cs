eb2f06ae2e4a25bfd14c5a135099d2fdd7415458
Jacob van't Hoog <vantjac@gmail.com>
2020-02-01 16:06:33 -0800

    extend format with metadata and confidence values

--- Assets/Scripts/ARRecord.cs
+++ Assets/Scripts/ARRecord.cs
@@ -44,13 +44,16 @@ public class ARRecord : MonoBehaviour
         string fileName = DateTime.Now.Ticks.ToString();
         filenameText.text = fileName;
         file = new StreamWriter(Application.persistentDataPath + "/" + fileName);
-        
-        startTime = Time.time;
 
         planesUpdated.Clear();
         planesRemoved.Clear();
         planesUpdated.AddRange(arOrigin.GetComponentsInChildren<ARPlane>());
         updatePointClouds = true;
+
+        Camera cam = arCam.GetComponent<Camera>();
+        file.WriteLine($"m {cam.fieldOfView} {Screen.width} {Screen.height}");
+
+        startTime = 0;
     }
 
     public void StopRecording()
@@ -83,22 +86,19 @@ public class ARRecord : MonoBehaviour
             timeText.text = "";
             return;
         }
+        if (startTime == 0)
+        {
+            startTime = Time.time;
+            return;
+        }
         string t = (Time.time - startTime).ToString();
         timeText.text = t;
-        file.WriteLine("t " + t);
+        file.WriteLine($"t {t}");
 
         Vector3 camPos = arCam.position;
         Quaternion camRot = arCam.rotation;
-        file.WriteLine(String.Format("c {0} {1} {2} {3} {4} {5} {6}",
-            camPos.x, camPos.y, camPos.z, camRot.x, camRot.y, camRot.z, camRot.w));
-        
-        foreach (ARReferencePoint refPoint in arOrigin.GetComponentsInChildren<ARReferencePoint>())
-        {
-            Vector3 pos = refPoint.transform.position;
-            Quaternion rot = refPoint.transform.rotation;
-            file.WriteLine(String.Format("r {0} {1} {2} {3} {4} {5} {6} {7}",
-                refPoint.trackableId.ToString(), pos.x, pos.y, pos.z, rot.x, rot.y, rot.z, rot.w));
-        }
+        file.WriteLine($"c {camPos.x} {camPos.y} {camPos.z} {camRot.x} {camRot.y} {camRot.z} {camRot.w}");
+
         if (updatePointClouds)
         {
             foreach (ARPointCloud pointCloud in arOrigin.GetComponentsInChildren<ARPointCloud>())
@@ -108,7 +108,8 @@ public class ARRecord : MonoBehaviour
                 {
                     Vector3 pos = pointCloud.positions[i];
                     ulong id = pointCloud.identifiers[i];
-                    file.WriteLine(String.Format("d {0} {1} {2} {3}", id, pos.x, pos.y, pos.z));
+                    float confidence = pointCloud.confidenceValues[i];
+                    file.WriteLine($"d {id} {pos.x} {pos.y} {pos.z} {confidence}");
                 }
             }
             updatePointClouds = false;
@@ -119,17 +120,17 @@ public class ARRecord : MonoBehaviour
                 continue;
             Vector3 pos = plane.transform.position;
             Quaternion rot = plane.transform.rotation;
-            file.WriteLine(String.Format("p {0} {1} {2} {3} {4} {5} {6} {7}",
-                plane.trackableId.ToString(), pos.x, pos.y, pos.z, rot.x, rot.y, rot.z, rot.w));
+            var id = plane.trackableId;
+            file.WriteLine($"p {id} {pos.x} {pos.y} {pos.z} {rot.x} {rot.y} {rot.z} {rot.w}");
             foreach (Vector2 point in plane.boundary)
             {
-                file.WriteLine(String.Format("b {0} {1}", point.x, point.y));
+                file.WriteLine($"b {point.x} {point.y}");
             }
         }
         planesUpdated.Clear();
         foreach (ARPlane plane in planesRemoved)
         {
-            file.WriteLine("pd " + plane.trackableId.ToString());
+            file.WriteLine($"pd {plane.trackableId}");
         }
         planesRemoved.Clear();
     }
