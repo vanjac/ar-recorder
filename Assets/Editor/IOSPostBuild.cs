e5be831975e793d581137c8d4d5b9cca1fda91f5
Jacob van't Hoog <vantjac@gmail.com>
2020-02-06 02:18:15 -0800

    Allow accessing files on ios

---
+++ Assets/Editor/IOSPostBuild.cs
@@ -0,0 +1,34 @@
+﻿#if UNITY_IOS
+
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.iOS.Xcode;
+using UnityEditor.Callbacks;
+
+
+public static class IOSPostBuild
+{
+    [PostProcessBuild]
+    public static void ChangeXcodePlist(BuildTarget buildTarget, string pathToBuiltProject)
+    {
+        if (buildTarget == BuildTarget.iOS)
+        {
+            Debug.Log("iOS post build");
+            string plistPath = pathToBuiltProject + "/Info.plist";
+            PlistDocument plist = new PlistDocument();
+            plist.ReadFromFile(plistPath);
+
+            PlistElementDict rootDict = plist.root;
+
+            rootDict.SetBoolean("UIFileSharingEnabled", true);
+            rootDict.SetBoolean("LSSupportsOpeningDocumentsInPlace", true);
+
+            plist.WriteToFile(plistPath);
+        }
+    }
+}
+
+#endif
