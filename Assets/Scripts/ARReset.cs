edcec0a177bdb39840cc099fd34e23be303531e5
Jacob van't Hoog <vantjac@gmail.com>
2020-02-16 00:12:15 -0800

    reset AR on unpause

---
+++ Assets/Scripts/ARReset.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class ARReset : MonoBehaviour
+{
+    public ARSession session;
+
+    void OnApplicationPause(bool pauseStatus)
+    {
+        if (!pauseStatus)
+        {
+            session.Reset();
+        }
+    }
+}
